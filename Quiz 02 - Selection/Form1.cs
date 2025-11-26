36779c0406475d48b07b214665f564c46b8d06fe
Cynthia Stone <cmstone24@hotmail.com>
2025-11-26 11:01:11 -0500

    scaling

--- Quiz 02 - Selection/Form1.cs
+++ Quiz 02 - Selection/Form1.cs
@@ -10,7 +10,7 @@ namespace Quiz_02___Selection
 
         private void q04InstructionButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("The user will enter the temperature in the first text box and answer if they brought a jacket in the second text box.  The output will be:\r\n\r\n“You should be comfortable” if If they brought a jacket and the temperature is below 15 , or they did not bring a jacket and the temperature is above 15\r\n\r\nIf they brought a jacket and the temperature is above 15 “You will be too hot”\r\n\r\nIf they did not bring a jacket and the temperature is below 15 “You are going to be cold”\r\n\r\nMake sure you add a try catch so there are no runtime errors.\r\n  For full marks use exactly three if statements ");
+            MessageBox.Show("The user will  enter the temperature in the first text box and answer if they brought a jacket in the second text box.  The output will be:\r\n\r\n“You should be comfortable” if If they brought a jacket and the temperature is below 15 , or they did not bring a jacket and the temperature is above 15\r\n\r\nIf they brought a jacket and the temperature is above 15 “You will be too hot”\r\n\r\nIf they did not bring a jacket and the temperature is below 15 “You are going to be cold”\r\n\r\nMake sure you add a try catch so there are no runtime errors.\r\n  For full marks use exactly three if statements ");
         }
 
         private void label18_Click(object sender, EventArgs e)
