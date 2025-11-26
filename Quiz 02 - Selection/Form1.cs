4472ed93b96b289ec7e5a6f1721511af0ef57453
Cynthia Stone <cmstone24@hotmail.com>
2025-11-26 11:08:53 -0500

    scale3

--- Quiz 02 - Selection/Form1.cs
+++ Quiz 02 - Selection/Form1.cs
@@ -4,8 +4,9 @@ namespace Quiz_02___Selection
     {
         public Form1()
         {
-            InitializeComponent();
             this.AutoScaleMode = AutoScaleMode.Font;
+            InitializeComponent();
+            
         }
 
         private void q04InstructionButton_Click(object sender, EventArgs e)
